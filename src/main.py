81d9b6243484dda97a0db6cfe5f75f8298a3e44d
horse-3903 <chongchoonhourafael@gmail.com>
2024-03-01 09:04:03 +0800

    v0.1.0
    main file

---
+++ src/main.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+from typing import *
+
+import tempfile
+
+from tqdm import tqdm
+
+class Checkpoint:
+    def __init__(self) -> None:
+        pass
+
+class CheckpointSaver:
+    def __init__(self, function: function, save_per: int) -> None:
+        self.function = function
+        self.it = 0
+        self.save_per = save_per
+        self.file = tempfile.NamedTemporaryFile("w+")
+
+    def call_function(self, *args):
+        self.function(*args)
+
+    def iterate(self, *args):
+        obj = self.call_function(*args)
+
+        self.it += 1
+    
+    def iterate_for(self, args_list: Iterator):
+        for _ in args_list:
+            self.call_function(args_list)
+
+c = CheckpointSaver(lambda x: print(x * 2), 2)
+c.call_function(3)
