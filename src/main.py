6987312e035451c9a6668df1348afd406617122e
horse-3903 <chongchoonhourafael@gmail.com>
2024-03-01 09:18:40 +0800

    v0.1.2

--- src/main.py
+++ src/main.py
@@ -10,9 +10,10 @@ import tempfile
 from tqdm import tqdm
 
 class Checkpoint:
-    def __init__(self, it: int, *kwargs) -> None:
+    def __init__(self, it: int, output: Any, *kwargs) -> None:
         self.timestamp = datetime.now()
         self.it = it
+        self.output = output
         self.variables = kwargs
 
 class CheckpointSaver:
@@ -25,14 +26,23 @@ class CheckpointSaver:
     def call_function(self, *args):
         self.function(*args)
 
+    def set_tqdm(self, desc: str = None, total: int = None, miniters: int = None, disable: bool = None, unit: str = None):
+        self.tqdm = {
+            "desc": desc,
+            "total": total,
+            "miniters": miniters,
+            "disable": disable,
+            "unit": unit,
+        }
+
     def iterate(self, *args):
         obj = self.call_function(*args)
 
         self.it += 1
     
     def iterate_for(self, args_list: Iterator):
-        for _ in args_list:
+        for _ in tqdm(args_list):
             self.call_function(args_list)
 
-c = CheckpointSaver(lambda x: print(x * 2), 2)
-c.call_function(3)
+# c = CheckpointSaver(lambda x: print(x * 2), 2)
+# c.call_function(3)
